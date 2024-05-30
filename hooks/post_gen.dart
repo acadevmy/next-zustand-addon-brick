12ba363908d04b9eb689e84731de0c1e96ffbc83
Gabriele Bilello <gabriele.bilello1@gmail.com>
2024-05-30 11:10:30 +0200

    feat: moved postgen scripts to pregen

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -1,16 +1,3 @@
-import 'dart:io';
-
 import 'package:mason/mason.dart';
 
-void run(HookContext context) async {
-  await runPnpm(context: context);
-}
-
-Future<void> runPnpm({
-  required HookContext context,
-}) async {
-  context.logger.info('📦 Running pnpm i');
-  await Process.run(
-      'pnpm', ['i', 'zustand']);
-  context.logger.success('📦 pnpm configured successfully 🚀');
-}
+void run(HookContext context) async {}
--- hooks/pre_gen.dart
+++ hooks/pre_gen.dart
@@ -1,3 +1,18 @@
+import 'dart:io';
+
 import 'package:mason/mason.dart';
 
-void run(HookContext context) {}
+Future<void> run(HookContext context) async {
+  await runPnpm(context: context);
+}
+
+Future<void> runPnpm({
+  required HookContext context,
+}) async {
+  context.logger.info('📦 Installing Zustand');
+  await Process.run(
+    'pnpm',
+    ['i', 'zustand'],
+  );
+  context.logger.success('📦 Zustand installed successfully 🚀');
+}
